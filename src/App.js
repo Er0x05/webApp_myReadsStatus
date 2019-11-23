96016b97661d4d45f5924f80237ba1d31f43c446
pek <pek.workit@gmail.com>
2019-11-23 19:06:42 +0800

    add module 'ShowShelf' n 'ShowBook'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import './App.css'
-import * as BooksAPI from './BooksAPI';
+import React from 'react';
 import { Link, Route } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+
+import './App.css';
+import ShowShelf from './ShowShelf';
 
 class BooksApp extends React.Component {
   state = {
@@ -25,13 +27,11 @@ class BooksApp extends React.Component {
             shelf: book.shelf
           }
         ));
-        console.log(getInfo)
         this.setState({
           reading:getInfo.filter(item=>item.shelf==="currentlyReading"),
           toRead:getInfo.filter(item=>item.shelf==="wantToRead"),
           read:getInfo.filter(item=>item.shelf==="read")
         });
-        console.log(this.state)
       })
   }
 
@@ -45,50 +45,19 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
-                        </div>
-                      </li>
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">Ender's Game</div>
-                          <div className="book-authors">Orson Scott Card</div>
-                        </div>
-                      </li>
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
+                <ShowShelf 
+                  title="Currently Reading"
+                  books={this.state.reading}
+                />
+                <ShowShelf 
+                  title="Want to Read"
+                  books={this.state.toRead}
+                />
+                <ShowShelf 
+                  title="Read"
+                  books={this.state.read}
+                />
+                {/* <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
@@ -189,9 +158,9 @@ class BooksApp extends React.Component {
                           <div className="book-authors">Mark Twain</div>
                         </div>
                       </li>
-                    </ol>
-                  </div>
-                </div>
+                    </ol> */}
+                  {/* </div>
+                </div> */}
               </div>
             </div>
             <div className="open-search">
